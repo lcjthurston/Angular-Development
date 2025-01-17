c43599e8c0b49d6a8ac8b5890dc560b948fdeeae
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-17 16:14:10 -0600

    task17

--- 01-starting-project-main/src/app/user/user.component.ts
+++ 01-starting-project-main/src/app/user/user.component.ts
@@ -1,24 +1,31 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
- 
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+
+// type User = {
+//     id: string;
+//     avatar: string;
+//     name: string;
+// }
+
+interface User {
+    id: string;
+    avatar: string;
+    name: string;
+}
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required:true }) id!:string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter<string>();
-  // select = output<string>();
+    @Input({ required: true }) user!: User;
+    @Output() select = new EventEmitter();
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+    get imagePath() {
+        return 'assets/users/' + this.user.avatar;
+    }
 
-  onSelectUser() {
-    this.select.emit(this.id)
-  }
+    onSelectUser() {
+        this.select.emit(this.user.id);
+    }
 }
