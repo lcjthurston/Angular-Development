da50ab14b76a0ba7b5b7dbd30093e343e72da16f
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-22 14:21:32 -0600

    Finished dashboard item component

--- 02-starting-project/src/app/app.component.ts
+++ 02-starting-project/src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { HeaderComponent } from './header/header.component';
-import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
-import { TrafficComponent } from './dashboard/traffic/traffic.component';
-import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { HeaderComponent } from "./header/header.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent]
+    selector: "app-root",
+    standalone: true,
+    templateUrl: "./app.component.html",
+    imports: [
+        HeaderComponent,
+        ServerStatusComponent,
+        TrafficComponent,
+        TicketsComponent,
+        DashboardItemComponent,
+    ],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
