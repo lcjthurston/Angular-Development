d6c58d0469049e524235db32d2a5f89ae976751d
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-23 11:54:45 -0600

    Added Button Component

---
+++ 02-starting-project/src/app/shared/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton]', // can also use button.button
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+    
+}
