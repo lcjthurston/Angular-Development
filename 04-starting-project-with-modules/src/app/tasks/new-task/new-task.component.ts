731f5d67b4c30a0ffc1b8b73a61708a70dbb329f
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-26 21:35:07 -0600

    migrated components to modules

--- 04-starting-project-with-modules/src/app/tasks/new-task/new-task.component.ts
+++ 04-starting-project-with-modules/src/app/tasks/new-task/new-task.component.ts
@@ -1,36 +1,33 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import { TasksService } from '../tasks.service';
+import { Component, EventEmitter, Input, Output, inject } from "@angular/core";
+import { TasksService } from "../tasks.service";
 
 @Component({
-  selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css',
+    selector: "app-new-task",
+    standalone: false,
+    templateUrl: "./new-task.component.html",
+    styleUrl: "./new-task.component.css",
 })
 export class NewTaskComponent {
-  @Input({ required: true }) userId!: string;
-  @Output() close = new EventEmitter<void>();
-  enteredTitle = '';
-  enteredSummary = '';
-  enteredDate = '';
-  private tasksService = inject(TasksService);
+    @Input({ required: true }) userId!: string;
+    @Output() close = new EventEmitter<void>();
+    enteredTitle = "";
+    enteredSummary = "";
+    enteredDate = "";
+    private tasksService = inject(TasksService);
 
-  onCancel() {
-    this.close.emit();
-  }
+    onCancel() {
+        this.close.emit();
+    }
 
-  onSubmit() {
-    this.tasksService.addTask(
-      {
-        title: this.enteredTitle,
-        summary: this.enteredSummary,
-        date: this.enteredDate,
-      },
-      this.userId
-    );
-    this.close.emit();
-  }
+    onSubmit() {
+        this.tasksService.addTask(
+            {
+                title: this.enteredTitle,
+                summary: this.enteredSummary,
+                date: this.enteredDate,
+            },
+            this.userId
+        );
+        this.close.emit();
+    }
 }
