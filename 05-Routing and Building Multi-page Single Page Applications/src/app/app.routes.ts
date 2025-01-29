5689615803a08aa16bc2a918a7ce226c2cc3dbdd
Luke Thurston <90153234+lcjthurston@users.noreply.github.com>
2025-01-29 10:18:48 -0600

    Added Not-Found Component

--- 05-Routing and Building Multi-page Single Page Applications/src/app/app.routes.ts
+++ 05-Routing and Building Multi-page Single Page Applications/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from "@angular/router";
+
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
     {
@@ -8,7 +12,21 @@ export const routes: Routes = [
         component: NoTaskComponent
     },
     {
-        path: "tasks", // <your-domain>/tasks
-        component: TasksComponent,
+        path: 'users/:userId', // <your-domain>/users/<uid>
+        component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks', // <your-domain>/users/<uid>/tasks
+                component: TasksComponent
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent
+            }
+        ]
     },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    }
 ];
--- 05-Routing and Building Multi-page Single Page Applications/src/app/users/user/user.component.ts
+++ 05-Routing and Building Multi-page Single Page Applications/src/app/users/user/user.component.ts
@@ -1,10 +1,13 @@
 import { Component, computed, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { type User } from './user.model';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
